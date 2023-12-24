051cdc363441704f7c01dccaf6f3e9d00a179c8f
Keiran <keinn2210@gmail.com>
2023-12-24 14:33:27 +0800

    added dummy products

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,25 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    title: 'Test',
+    price: 6,
+    description:'This is a first product - amazing!'
+  },
+  {
+    title: 'Book',
+    price: 12,
+    description:'This is a book'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product)=> <ProductItem key={product.title} title={product.title} price={product.price} description={product.description}/>)}
       </ul>
     </section>
   );
